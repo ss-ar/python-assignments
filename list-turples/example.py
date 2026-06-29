d28c5dee0944e158f958ab1d957ea705d0c6bbab
Timothy-Office <ar.ssewa@outlook.com>
2026-06-29 16:41:33 +0300

    Practicing turples

---
+++ list-turples/example.py
@@ -0,0 +1,7 @@
+# inventory = ["Sword","Shield","Potion"]
+# print(inventory[-1])
+# inventory.append("Map")
+# print(inventory[-1])
+kampala_cordinates = (0.3136, 32.5811)
+print(f"Latitude is: {kampala_cordinates[-1]}")
+kampala_cordinates[0] = 1.500
