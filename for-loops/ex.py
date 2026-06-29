95230a10a10921a0cfd0a592c9af2214417ba203
Timothy-Office <ar.ssewa@outlook.com>
2026-06-29 16:42:18 +0300

    Created examples and some challenges in for-loop

---
+++ for-loops/ex.py
@@ -0,0 +1,66 @@
+# playlist = ["Track 1","Lo-Fi Beats","Acoustic Sunset"]
+# for song in playlist:
+#   print("Now playing: ",song)
+
+# print("Starting the countdown...")
+# for i in range(5):
+#   print(i)
+
+# total_viewa = 0
+# daily_views = [100,250,50]
+
+# for views in daily_views:
+#   total_viewa = total_viewa+views
+
+# print("Total weekly views: ",total_viewa)
+# import time  # This brings in the time module so we can pause the loop!
+
+# print("Initiating launch sequence...")
+# time.sleep(1) # Pauses for 1 second before starting
+
+# for i in range(20):
+#     # 'i' goes from 0 to 19. 
+#     # We multiply a blank space by 'i' to push the ship further right
+#     spaces = " " * i  
+    
+#     print(spaces + ">")
+    
+#     # Pause for 0.2 seconds before the loop repeats
+#     time.sleep(0.2) 
+
+# print("The ship has left the galaxy!")
+# import time
+
+# print("Downloading new anime episode...")
+
+# for i in range(1, 11):
+#     # We create a bar by multiplying a symbol by the loop number (i)
+#     bar = "█" * i
+    
+#     # We calculate the percentage (10, 20, 30...)
+#     percentage = i * 10 
+    
+#     # Print the bar and the percentage
+#     print(f"{bar} {percentage}%")
+    
+#     # Pause for half a second so it looks like it's actually downloading
+#     time.sleep(0.5)
+
+# print("Download Complete! Enjoy the show.")
+
+wild_animals = ["Hippo","Lion","Zebra","Tiger"]
+for animals in wild_animals:
+    print(animals)
+
+for i in range(5):
+    print(i)
+
+sum = 0
+avg = 0
+marks = [50,60,70,80]
+for mark in marks:
+    sum += mark
+    avg = sum/4
+
+print(sum)
+print(avg)
---
+++ for-loops/gallery_labels.py
@@ -0,0 +1,3 @@
+artworks=["Neon","Samurai","Forest Spirit","Cyber City","Lo-Fi Room"]
+for artwork in artworks:
+  print(f"*** NOW VIEWING: {artwork} ***")
