392934c2263627342ed6bb98505ad4096cc8babc
Timothy-Office <ar.ssewa@outlook.com>
2026-07-01 10:36:03 +0300

    Just another for loop examples

---
+++ for-loops/render_bar.py
@@ -0,0 +1,4 @@
+progress_bar = ""
+for bar in range(10):
+  progress_bar += "#"
+  print(f"Rendering: {progress_bar}")
---
+++ for-loops/spootify_stats.py
@@ -0,0 +1,6 @@
+monthly_listeners = [500,1200,800,2000,1500]
+total_listeners = 0
+for listeners in monthly_listeners:
+  total_listeners += listeners
+  print(f"Month  complete! Total is now {total_listeners}")
+
